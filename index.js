1d4619d2ce36082eeb7da39db37daf7a7603fc20
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2020-02-01 02:41:36 +0100

    Removed 'readable' event, otherwise 'data' event is never triggered

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const EventEmitter = require('events').EventEmitter
 class ChunkedBuffer extends EventEmitter {
   constructor (readStream, opts) {
     super()
-    this.readStream = readStream
-    this.opts = opts
-    this.bindEvents()
-  }
-
-  bindEvents () {
-    const { readStream, opts } = this
     readStream.on('data', buffer => {
       for (let start = 0;start < buffer.byteLength;start+=opts.chunkSize) {
         const chunk = buffer.slice(start, start + opts.chunkSize)
@@ -20,7 +13,6 @@ class ChunkedBuffer extends EventEmitter {
     readStream.on('close', () => this.emit('close'))
     readStream.on('pause', () => this.emit('pause'))
     readStream.on('resume', () => this.emit('resume'))
-    readStream.on('readable', () => this.emit('readable'))
     readStream.on('error', (err) => this.emit('error', err))
   }
 }
